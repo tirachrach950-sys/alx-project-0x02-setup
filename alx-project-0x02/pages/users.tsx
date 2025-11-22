ac0ec1934c5dd9d394fe29e1e07005a56e1d7b37
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-22 15:11:38 +0100

    Update users.tsx

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,1 +1,14 @@
+import { type UserProps } from "@/interfaces";
+
+export default function UserCard({ name, email, address }: UserProps) {
+  return (
+    <div className="border rounded-lg shadow-md p-6 m-4 bg-white w-80 hover:shadow-xl transition">
+      <h2 className="text-xl font-bold mb-2">{name}</h2>
+      <p className="text-gray-700 mb-1">{email}</p>
+      <p className="text-gray-500 text-sm">
+        {address.street}, {address.suite}, {address.city}, {address.zipcode}
+      </p>
+    </div>
+  );
+}
 
