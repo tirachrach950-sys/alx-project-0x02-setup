323fe3fe0f7cf9845680afaa2e55edca7a19f44c
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-22 14:28:56 +0100

    Update home.tsx

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,1 +1,10 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-10">
+        Welcome to the Home Page
+      </h1>
+    </div>
+  );
+}
 
