8b81760a2a1aab1089559f6d481454b5d1b2a662
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-22 13:51:23 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,13 @@
+import Header from "@/components/layout/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h1 className="text-3xl font-bold text-center mt-10">
+        Welcome to ALX Project 2
+      </h1>
+    </div>
+  );
+}
 
