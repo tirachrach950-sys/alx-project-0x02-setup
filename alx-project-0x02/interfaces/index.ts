936c3bb8a5b3a890066c34c32eba820c2ca9bf76
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-22 14:49:23 +0100

    Update index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,10 +1,17 @@
+
 // Existing interfaces
 export interface ExampleInterface {
   message: string;
 }
 
-// Card interface
 export interface CardProps {
   title: string;
   content: string;
+}
+
+// New interface for Button
+export interface ButtonProps {
+  size: "small" | "medium" | "large";
+  shape: "rounded-sm" | "rounded-md" | "rounded-full";
+  title: string;
 }
