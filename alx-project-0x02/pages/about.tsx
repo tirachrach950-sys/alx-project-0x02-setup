37783688f7cb020f3a52df92a8a9cddbcceea45f
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-22 14:50:32 +0100

    Update about.tsx

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -7,4 +7,17 @@ export default function AboutPage() {
     </div>
   );
 }
+import Button from "@/components/common/Button";
+
+export default function AboutPage() {
+  return (
+    <div className="p-8 flex flex-col items-center gap-4">
+      <h1 className="text-3xl font-bold mb-6">About Us Page</h1>
+
+      <Button size="small" shape="rounded-sm" title="Small Rounded" />
+      <Button size="medium" shape="rounded-md" title="Medium Rounded" />
+      <Button size="large" shape="rounded-full" title="Large Rounded" />
+    </div>
+  );
+}
 
