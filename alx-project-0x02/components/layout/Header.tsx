8f576f6986c5b67e9f4d423d5b7b431fe6f6b0e5
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-22 14:29:51 +0100

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <header className="w-full p-4 bg-blue-600 text-white text-center">
-      <h2 className="text-xl font-semibold">ALX Project Header</h2>
+    <header className="w-full p-4 bg-blue-600 text-white flex justify-center gap-6">
+      <Link href="/home" className="text-lg hover:underline">
+        Home
+      </Link>
+      <Link href="/about" className="text-lg hover:underline">
+        About
+      </Link>
     </header>
   );
 }
-
