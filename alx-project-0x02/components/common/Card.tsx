c8a52bdcb594d96f7cd9f2c872d1c4c6278100ca
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-22 14:38:38 +0100

    Update Card.tsx

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,4 +1,4 @@
-import { CardProps } from "@/interfaces";
+import type { CardProps } from "@/interfaces";
 
 export default function Card({ title, content }: CardProps) {
   return (
@@ -8,4 +8,3 @@ export default function Card({ title, content }: CardProps) {
     </div>
   );
 }
-
