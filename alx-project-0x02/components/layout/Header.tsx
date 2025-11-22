2193a844d76fc0820a38d6e1135866cce6693e50
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-22 13:52:15 +0100

    Add Header component for ALX project

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,1 +1,8 @@
+export default function Header() {
+  return (
+    <header className="w-full p-4 bg-blue-600 text-white text-center">
+      <h2 className="text-xl font-semibold">ALX Project Header</h2>
+    </header>
+  );
+}
 
