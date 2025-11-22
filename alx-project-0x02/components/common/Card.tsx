97935046bceaa2c9d87abc24559442cb3b0b7226
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-22 14:35:12 +0100

    Update Card.tsx

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,1 +1,11 @@
+import { CardProps } from "@/interfaces";
+
+export default function Card({ title, content }: CardProps) {
+  return (
+    <div className="border rounded-lg shadow-md p-6 m-4 bg-white hover:shadow-xl transition">
+      <h2 className="text-xl font-bold mb-2">{title}</h2>
+      <p>{content}</p>
+    </div>
+  );
+}
 
