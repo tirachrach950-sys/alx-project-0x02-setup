abae778e15ebb31514d7d77bc96a6f1bfd92ab47
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-22 15:01:10 +0100

    Update posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,1 +1,15 @@
+import Header from "@/components/layout/Header";
+
+export default function PostsPage() {
+  return (
+    <div>
+      <Header />
+
+      <div className="p-8">
+        <h1 className="text-3xl font-bold text-center">Posts Page</h1>
+        <p className="text-center mt-4">This is the Posts page content.</p>
+      </div>
+    </div>
+  );
+}
 
